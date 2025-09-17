b44613a51b2176d63a7ace14d39c2a739aafbac6
Pontus Ågren Grundström <pontus.agrengrundstrom@gmail.com>
2025-09-17 11:58:26 +0200

    packaging base structure for frontend and backend

---
+++ src/taxipred/frontend/dashboard.py
@@ -0,0 +1,19 @@
+import streamlit as st
+from taxipred.utils.helpers import read_api_endpoint
+import pandas as pd
+
+st.set_page_config(layout="wide", page_title="Taxi Prediction Dashboard")
+
+data = read_api_endpoint("taxi")
+
+df = pd.DataFrame(data.json())
+
+
+def main():
+    st.markdown("# Taxi Prediction Dashboard")
+
+    st.dataframe(df)
+
+
+if __name__ == "__main__":
+    main()
