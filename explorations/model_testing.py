0269790410b2755b31e572660ff7217f4d052eae
Pontus Ågren Grundström <pontus.agrengrundstrom@gmail.com>
2025-09-24 11:35:20 +0200

    added function for testing out different hyperparameters for randomforest

--- explorations/model_testing.py
+++ explorations/model_testing.py
@@ -6,8 +6,41 @@ from xgboost import XGBRegressor
 from sklearn.linear_model import RidgeCV
 from sklearn.linear_model import ElasticNetCV
 from sklearn.preprocessing import StandardScaler, MinMaxScaler
-from sklearn.metrics import mean_absolute_error, mean_squared_error, root_mean_squared_error
+from sklearn.metrics import mean_absolute_error, mean_squared_error, root_mean_squared_error, r2_score
 import pandas as pd
+
+
+def randomforest_hyperparameters(X_train, X_val, y_train, y_val):
+    estimators = [100, 500, 1000]
+    depth = [None, 10, 20]
+    splits = [2, 5, 10]
+    leafs = [1, 2, 4]
+
+    model_scores = []
+    for e in estimators:
+        for d in depth:
+            for s in splits:
+                for l in leafs:
+                    model = RandomForestRegressor(n_estimators=e, max_depth=d, min_samples_split=s, min_samples_leaf=l, random_state=42)
+                    model.fit(X_train, y_train)
+                    
+                    y_pred = model.predict(X_val)
+                    rmse = root_mean_squared_error(y_val, y_pred)
+                    mae = mean_absolute_error(y_val, y_pred)
+                    r2 = r2_score(y_val, y_pred)
+                    
+                    model_scores.append({
+                        "n_estimators": e,
+                        "max_depth": d,
+                        "min_samples_split": s,
+                        "min_samples_leaf": l,
+                        "mae": mae,
+                        "rmse": rmse,
+                        "r2_score": r2
+                    })
+                    
+    return pd.DataFrame(model_scores).sort_values(by="rmse").reset_index(drop=True)
+
 
 
 def find_best_k(scaled_X_train, scaled_X_val, y_train, y_val):
@@ -23,6 +56,7 @@ def find_best_k(scaled_X_train, scaled_X_val, y_train, y_val):
     best_k = error_list.index(best_index)+1
     
     return best_k
+
 
 def test_models(df, target, test_size=0.2,
                 models = {
@@ -67,6 +101,27 @@ def test_models(df, target, test_size=0.2,
                 "mse": mse,
                 "rmse": rmse
             })
+            
+    for key, model in models.items():
+        if model == KNeighborsRegressor():
+                best_k = find_best_k(X_train, X_val, y_train, y_val)
+                model = KNeighborsRegressor(n_neighbors=best_k)
+                
+        model.fit(X_train, y_train)
+        y_pred = model.predict(X_val)
+        
+        mae = mean_absolute_error(y_val, y_pred)
+        mse = mean_squared_error(y_val, y_pred)
+        rmse = root_mean_squared_error(y_val, y_pred)
+        
+        result.append({
+            "model": key,
+            "scaler": "No Scaler",
+            "mae": mae,
+            "mse": mse,
+            "rmse": rmse
+        })
+    
     result_df = pd.DataFrame(result)
     
-    return result_df.sort_values(by="rmse")
+    return result_df.sort_values(by="rmse").reset_index(drop=True)
