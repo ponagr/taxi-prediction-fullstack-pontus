c10f485005242dda2c5ee9e70e054b0c421dfca6
Pontus Ågren Grundström <pontus.agrengrundstrom@gmail.com>
2025-09-24 16:34:10 +0200

    added paths to model and csv file

--- src/taxipred/utils/constants.py
+++ src/taxipred/utils/constants.py
@@ -4,6 +4,15 @@ from importlib.resources import files
 # får tillbaka ett resource-träd med alla filer i paketet (Traversable)
 # 'joinpath' skapar sedan en sökväg inne i paketet
 # och vår Traversable(resultatet) ser ut såhär: <project-root>/src/taxipred/data/taxi_trip_pricing.csv
-TAXI_CSV_PATH = files("taxipred").joinpath("data/taxi_trip_pricing.csv")
+
+# CSV PATHS
+# TAXI_ORIGINAL_PATH = files("taxipred").joinpath("data/taxi_trip_pricing.csv")
+TAXI_CSV_PATH = files("taxipred").joinpath("data/taxi_trip_pricing_cleaned_no_categorical.csv")
+# TAXI_WITH_CATEGORICAL_FEATURES_PATH = files("taxipred").joinpath("data/taxi_trip_pricing_cleaned.csv")
+# TAXI_MISSING_TARGET_PATH = files("taxipred").joinpath("data/missing_target_data.csv")
+
+# MODEL PATHS
+# TAXI_REGRESSOR_MODEL_WITH_CATEGORICAL_FEATURES = files("taxipred").joinpath("models/taxi_regressor_categorical.joblib")
+MODEL_PATH = files("taxipred").joinpath("models/taxi_regressor.joblib")
 
 # DATA_PATH = Path(__file__).parents[1] / "data"
