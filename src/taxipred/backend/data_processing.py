4e67eb451eddbc55e4487c6a8c656fc2c38997d2
Pontus Ågren Grundström <pontus.agrengrundstrom@gmail.com>
2025-10-01 12:40:58 +0200

    added df_original and function in TaxiData for converting it for api, and some cleanup of code and comments in the file

--- src/taxipred/backend/data_processing.py
+++ src/taxipred/backend/data_processing.py
@@ -1,15 +1,12 @@
-from taxipred.utils.constants import TAXI_CSV_PATH, TAXI_MISSING_TARGET_PATH#TAXI_WITH_CATEGORICAL_FEATURES_PATH, TAXI_MISSING_TARGET_PATH, TAXI_ORIGINAL_PATH
+from taxipred.utils.constants import TAXI_CSV_PATH, TAXI_MISSING_TARGET_PATH, TAXI_ORIGINAL_PATH
 import pandas as pd
 import json
 from pydantic import BaseModel, Field
 from typing import Literal
 
 
-# TODO: konvertera alla värden från USD till SEK?
-# TODO: göra om passenger count till int?
+
 # TODO: lägga in datan med missing values på Taxi_Price för att kunna klicka på och fylla värden från dashboarden
-# TODO: lägga till upphämtningsplats och avlämningsplats, koppla till nån api för maps, och automatiskt fylla i uppskattad distance och duration, endast fylla i rate per min/km, base fare och antal passagerare 
-# TODO: dashboard page för kund och för företag(där man vill maximera profit)
 # TODO: räkna ut procent score för model prediction, med rmse delat på medelvärde på nått sätt?
 # TODO: tvätta datan med lite mer random diff på mean och median
 
@@ -17,10 +14,11 @@ class TaxiData:
     def __init__(self):
         self.df = pd.read_csv(TAXI_CSV_PATH, index_col=0)
         self.missing_target = pd.read_csv(TAXI_MISSING_TARGET_PATH, index_col=0)
-        # self.df_categorical = pd.read_csv(TAXI_WITH_CATEGORICAL_FEATURES_PATH)
-        # self.df_missing_target = pd.read_csv(TAXI_MISSING_TARGET_PATH)
-        # self.df_original = pd.read_csv(TAXI_ORIGINAL_PATH)
+        self.df_original = pd.read_csv(TAXI_ORIGINAL_PATH)
 
+    def original_to_json(self):
+        return json.loads(self.df_original.to_json(orient="records"))
+    
     def missing_target_to_json(self):
         return json.loads(self.missing_target.to_json(orient="records"))
     
@@ -56,6 +54,3 @@ class TaxiFareOutput(BaseModel):
     Base_Fare: float = Field(ge=2, le=5)
     Per_Km_Rate: float = Field(ge=0.5, le=2)
     Per_Minute_Rate: float = Field(ge=0.1, le=0.5)
-    # Base_Fare: float
-    # Per_Km_Rate: float
-    # Per_Minute_Rate: float
