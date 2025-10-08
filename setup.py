126af83e3e21c3400aad3ace9c602088084f7967
Pontus Ågren Grundström <pontus.agrengrundstrom@gmail.com>
2025-10-08 09:57:29 +0200

    updated setup steps in readme, and added final dependency packages in setup.py

--- setup.py
+++ setup.py
@@ -14,11 +14,11 @@ setup(
     author="Pontus Ågren Grundström",
     author_email="pontus.agrengrundstrom@gmail.com",
     # paket som installeras automatiskt när man install taxipred. (dependencies för taxipred paketet)
-    install_requires=["streamlit", "pandas", "fastapi", "uvicorn", "pydantic", "dotenv"],     
+    install_requires=["streamlit", "pandas", "fastapi", "uvicorn", "pydantic", "python-dotenv", "joblib", "scikit-learn"],     
     # berättar att paketet 'taxipred' ligger i mappen src, för att setup inte ska leta efter taxipred i rooten där setup.py filen finns, alltså src/taxipred/
     package_dir={"": "src"},    
     # inkluderar filer som inte är .py filer, i detta fall, alla .csv filer som finns i src/taxipred/data, dessa filer följer med i paketet vid installation
     package_data={"taxipred": ["data/*.csv", "models/*.joblib", "assets/*.png"]},  
     # letar efter paket genom att kolla efter mappar med __init__.py 
-    packages=find_packages(),   
+    packages=find_packages(where="src"),   
 )
